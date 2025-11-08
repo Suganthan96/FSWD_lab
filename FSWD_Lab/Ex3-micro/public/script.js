5fc091551b17038b060309b181cbe4a67e496309
Suganthan96 <suganthan.27it@licet.ac.in>
2025-11-08 12:56:07 +0530

    ex3 added

--- FSWD_Lab/Ex3-micro/public/script.js
+++ FSWD_Lab/Ex3-micro/public/script.js
@@ -1,1 +1,8 @@
-sdzdvx
+fetch('/api/posts')
+  .then(res => res.json())
+  .then(data => {
+    const posts = document.getElementById('posts');
+    data.forEach(p => {
+      posts.innerHTML += `<h3>${p.title}</h3><p>${p.content}</p><hr>`;
+    });
+  });
